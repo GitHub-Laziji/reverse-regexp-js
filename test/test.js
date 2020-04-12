e81047a3e217d9b4df4c6f70e6d81bf79edc0bf9
GitHub-Laziji <c_smtp@163.com>
2020-04-12 21:46:22 +0800

    init

--- test/test.js
+++ test/test.js
@@ -1,6 +1,16 @@
 const Node = require("../dist/index").default;
 
-let node = Node.compile("\\w{6,12}@[a-z0-9]{3}\\.(com|cn)");
-for (let i = 0; i < 10; i++) {
-  console.log(node.random())
+function random(expression, title) {
+  console.log(`${title} ${expression}`);
+  let node = Node.compile(expression);
+  for (let i = 0; i < 10; i++) {
+    let data = node.random();
+    console.log(`[${new RegExp(`^(${expression})$`).test(data)}] ${data}`);
+  }
+  console.log();
 }
+
+random("\\w{6,12}@[a-z0-9]{3}\\.(com|cn)", "邮箱");
+random("1(3|5|7|8)\\d{9}", "手机号");
+random("-?[1-9]\\d*\\.\\d+", "浮点数");
+random("https?://[\\w-]+(\\.[\\w-]+){1,2}(/[\\w-]{3,6}){0,2}(\\?[\\w_]{4,6}=[\\w_]{4,6}(&[\\w_]{4,6}=[\\w_]{4,6}){0,2})?", "网址");
